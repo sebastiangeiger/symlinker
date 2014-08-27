e6c9f6f4d2e3d871a534b069a2d289e9e12204ea
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-08-27 15:52:57 -0400

    Renaming the test folders to line up with params
      * existing -> from
      * new -> to

--- symlinker_test.rb
+++ symlinker_test.rb
@@ -31,124 +31,124 @@ describe Symlinker do
     before(:each) {
       FileUtils.rm_rf("sandbox")
       FileUtils.mkdir("sandbox")
-      FileUtils.mkdir("sandbox/existing")
+      FileUtils.mkdir("sandbox/from")
     }
     let(:symlinker) do
-      Symlinker.new(from: "sandbox/existing", to: "sandbox/new", ui: ui)
+      Symlinker.new(from: "sandbox/from", to: "sandbox/to", ui: ui)
     end
     let(:ui) do
       MiniTest::Mock.new
     end
     it "symlinks files" do
-      ui.expect :linked, nil, [File.absolute_path("sandbox/existing/file"), File.absolute_path("sandbox/new/file")]
-      create_file "sandbox/existing/file"
+      ui.expect :linked, nil, [File.absolute_path("sandbox/from/file"), File.absolute_path("sandbox/to/file")]
+      create_file "sandbox/from/file"
       symlinker.link!
       ui.verify
-      IO.read("sandbox/new/file").must_equal "Created by create_file"
+      IO.read("sandbox/to/file").must_equal "Created by create_file"
     end
     it "symlinks directories" do
-      ui.expect :linked, nil, [File.absolute_path("sandbox/existing/dir"), File.absolute_path("sandbox/new/dir")]
-      create_dir "sandbox/existing/dir"
+      ui.expect :linked, nil, [File.absolute_path("sandbox/from/dir"), File.absolute_path("sandbox/to/dir")]
+      create_dir "sandbox/from/dir"
       symlinker.link!
       ui.verify
-      File.symlink?("sandbox/new/dir").must_equal true
+      File.symlink?("sandbox/to/dir").must_equal true
     end
     it "symlinks nested directories" do
-      ui.expect :linked, nil, [File.absolute_path("sandbox/existing/dir"), File.absolute_path("sandbox/new/dir")]
-      create_dir  "sandbox/existing/dir"
-      create_file "sandbox/existing/dir/file"
+      ui.expect :linked, nil, [File.absolute_path("sandbox/from/dir"), File.absolute_path("sandbox/to/dir")]
+      create_dir  "sandbox/from/dir"
+      create_file "sandbox/from/dir/file"
       symlinker.link!
       ui.verify
-      IO.read("sandbox/new/dir/file").must_equal "Created by create_file"
+      IO.read("sandbox/to/dir/file").must_equal "Created by create_file"
     end
     describe 'when the file already exists' do
       before(:each) do
-        create_dir  "sandbox/new"
-          create_file "sandbox/existing/file", "File to be linked"
+        create_dir  "sandbox/to"
+          create_file "sandbox/from/file", "File to be linked"
       end
       describe 'and they are different' do
         before(:each) do
-          create_file "sandbox/new/file", "I was here first"
+          create_file "sandbox/to/file", "I was here first"
         end
         it 'asks the user how to proceed' do
-          ui.expect :file_exists, :dont_overwrite, [File.absolute_path("sandbox/new/file")]
-          ui.expect :skipped, :nil, [File.absolute_path("sandbox/new/file")]
+          ui.expect :file_exists, :dont_overwrite, [File.absolute_path("sandbox/to/file")]
+          ui.expect :skipped, :nil, [File.absolute_path("sandbox/to/file")]
           symlinker.link!
           ui.verify
         end
         it 'does not touch the file if user says to not overwrite' do
-          ui.expect :file_exists, :dont_overwrite, [File.absolute_path("sandbox/new/file")]
-          ui.expect :skipped, :nil, [File.absolute_path("sandbox/new/file")]
+          ui.expect :file_exists, :dont_overwrite, [File.absolute_path("sandbox/to/file")]
+          ui.expect :skipped, :nil, [File.absolute_path("sandbox/to/file")]
           symlinker.link!
           ui.verify
-          IO.read("sandbox/new/file").must_equal "I was here first"
+          IO.read("sandbox/to/file").must_equal "I was here first"
         end
         it 'changes file if user says to overwrite' do
-          ui.expect :file_exists, :overwrite, [File.absolute_path("sandbox/new/file")]
-          ui.expect :overwritten, :nil, [File.absolute_path("sandbox/existing/file"),File.absolute_path("sandbox/new/file")]
+          ui.expect :file_exists, :overwrite, [File.absolute_path("sandbox/to/file")]
+          ui.expect :overwritten, :nil, [File.absolute_path("sandbox/from/file"),File.absolute_path("sandbox/to/file")]
           symlinker.link!
           ui.verify
-          IO.read("sandbox/new/file").must_equal "File to be linked"
+          IO.read("sandbox/to/file").must_equal "File to be linked"
         end
       end
       describe 'and they are identical' do
         before(:each) do
-          link_file "sandbox/existing/file", "sandbox/new/file"
+          link_file "sandbox/from/file", "sandbox/to/file"
         end
         it 'does not ask the user' do
-          ui.expect :identical, nil, [File.absolute_path("sandbox/new/file")]
+          ui.expect :identical, nil, [File.absolute_path("sandbox/to/file")]
           symlinker.link!
           ui.verify
-          IO.read("sandbox/new/file").must_equal "File to be linked"
+          IO.read("sandbox/to/file").must_equal "File to be linked"
         end
       end
     end
     describe 'with erb files' do
       describe 'when the file does not exist' do
         it 'expands the file first' do
-          ui.expect :generated, nil, [File.absolute_path("sandbox/existing/file.erb"),File.absolute_path("sandbox/new/file")]
+          ui.expect :generated, nil, [File.absolute_path("sandbox/from/file.erb"),File.absolute_path("sandbox/to/file")]
           ENV['variable'] = "World"
-          create_file "sandbox/existing/file.erb", "Hello, <%= ENV['variable'] %>!"
+          create_file "sandbox/from/file.erb", "Hello, <%= ENV['variable'] %>!"
           symlinker.link!
           ui.verify
-          IO.read("sandbox/new/file").must_equal "Hello, World!"
+          IO.read("sandbox/to/file").must_equal "Hello, World!"
         end
       end
       describe 'when the file exists' do
         before(:each) do
-          create_dir "sandbox/new"
-          create_file "sandbox/new/file", "Hello, World!", mtime: Time.now - 3660
+          create_dir "sandbox/to"
+          create_file "sandbox/to/file", "Hello, World!", mtime: Time.now - 3660
         end
         describe 'and they are not identical' do
           before(:each) do
             ENV['variable'] = "Universe"
-            create_file "sandbox/existing/file.erb", "Hey, <%= ENV['variable'] %>!"
+            create_file "sandbox/from/file.erb", "Hey, <%= ENV['variable'] %>!"
           end
           it 'overrides when the chooses to' do
-            ui.expect :erb_files_differ, :overwrite, [File.absolute_path("sandbox/new/file"), "Hello, World!", "Hey, Universe!"]
-            ui.expect :overwritten, nil, [File.absolute_path("sandbox/existing/file.erb"),File.absolute_path("sandbox/new/file")]
+            ui.expect :erb_files_differ, :overwrite, [File.absolute_path("sandbox/to/file"), "Hello, World!", "Hey, Universe!"]
+            ui.expect :overwritten, nil, [File.absolute_path("sandbox/from/file.erb"),File.absolute_path("sandbox/to/file")]
             symlinker.link!
             ui.verify
-            IO.read("sandbox/new/file").must_equal "Hey, Universe!"
-            File.mtime("sandbox/new/file").must_be_close_to Time.now, 100
+            IO.read("sandbox/to/file").must_equal "Hey, Universe!"
+            File.mtime("sandbox/to/file").must_be_close_to Time.now, 100
           end
           it 'does not override when the chooses to' do
-            ui.expect :erb_files_differ, :dont_overwrite, [File.absolute_path("sandbox/new/file"), "Hello, World!", "Hey, Universe!"]
-            ui.expect :skipped, nil, [File.absolute_path("sandbox/new/file")]
+            ui.expect :erb_files_differ, :dont_overwrite, [File.absolute_path("sandbox/to/file"), "Hello, World!", "Hey, Universe!"]
+            ui.expect :skipped, nil, [File.absolute_path("sandbox/to/file")]
             symlinker.link!
             ui.verify
-            IO.read("sandbox/new/file").must_equal "Hello, World!"
-            File.mtime("sandbox/new/file").must_be_close_to Time.now - 3600, 100
+            IO.read("sandbox/to/file").must_equal "Hello, World!"
+            File.mtime("sandbox/to/file").must_be_close_to Time.now - 3600, 100
           end
         end
         it 'does not do anything if they are identical' do
-          ui.expect :identical, nil, [File.absolute_path("sandbox/existing/file.erb"),File.absolute_path("sandbox/new/file")]
+          ui.expect :identical, nil, [File.absolute_path("sandbox/from/file.erb"),File.absolute_path("sandbox/to/file")]
           ENV['variable'] = "World"
-          create_file "sandbox/existing/file.erb", "Hello, <%= ENV['variable'] %>!"
+          create_file "sandbox/from/file.erb", "Hello, <%= ENV['variable'] %>!"
           symlinker.link!
           ui.verify
-          IO.read("sandbox/new/file").must_equal "Hello, World!"
-          File.mtime("sandbox/new/file").must_be_close_to Time.now - 3600, 100
+          IO.read("sandbox/to/file").must_equal "Hello, World!"
+          File.mtime("sandbox/to/file").must_be_close_to Time.now - 3600, 100
         end
       end
     end
@@ -167,39 +167,39 @@ describe SymlinkerUI do
   let(:input_stream) { MiniTest::Mock.new }
   describe '#linked' do
     it 'prints the right message' do
-      ui.linked(File.absolute_path("sandbox/existing/file"),File.absolute_path("sandbox/new/file"))
-      output_stream.string.must_equal "sandbox/new/file: Linked to sandbox/existing/file\n"
+      ui.linked(File.absolute_path("sandbox/from/file"),File.absolute_path("sandbox/to/file"))
+      output_stream.string.must_equal "sandbox/to/file: Linked to sandbox/from/file\n"
     end
   end
   describe '#overwritten' do
     it 'prints the right message' do
-      ui.overwritten(File.absolute_path("sandbox/existing/file"),File.absolute_path("sandbox/new/file"))
-      output_stream.string.must_equal "sandbox/new/file: Linked to sandbox/existing/file [Overwritten]\n"
+      ui.overwritten(File.absolute_path("sandbox/from/file"),File.absolute_path("sandbox/to/file"))
+      output_stream.string.must_equal "sandbox/to/file: Linked to sandbox/from/file [Overwritten]\n"
     end
   end
   describe '#skipped' do
     it 'prints the right message' do
-      ui.skipped(File.absolute_path("sandbox/new/file"))
-      output_stream.string.must_equal "sandbox/new/file: Skipped\n"
+      ui.skipped(File.absolute_path("sandbox/to/file"))
+      output_stream.string.must_equal "sandbox/to/file: Skipped\n"
     end
   end
   describe '#identical' do
     it 'prints the right message' do
-      ui.identical(File.absolute_path("sandbox/new/file"))
-      output_stream.string.must_equal "sandbox/new/file: Identical\n"
+      ui.identical(File.absolute_path("sandbox/to/file"))
+      output_stream.string.must_equal "sandbox/to/file: Identical\n"
     end
   end
   describe '#generated' do
     it 'prints the right message' do
-      ui.generated(File.absolute_path("sandbox/existing/file.erb"),File.absolute_path("sandbox/new/file"))
-      output_stream.string.must_equal "sandbox/new/file: Generated from sandbox/existing/file.erb\n"
+      ui.generated(File.absolute_path("sandbox/from/file.erb"),File.absolute_path("sandbox/to/file"))
+      output_stream.string.must_equal "sandbox/to/file: Generated from sandbox/from/file.erb\n"
     end
   end
   describe '#erb_files_differ' do
     it 'prints the right thing' do
       input_stream.expect :gets, "y\n"
-      ui.erb_files_differ(File.absolute_path("sandbox/new/file"), "Existing", "New")
-      output_stream.string.must_include "Overwrite sandbox/new/file? [y/n] "
+      ui.erb_files_differ(File.absolute_path("sandbox/to/file"), "Existing", "New")
+      output_stream.string.must_include "Overwrite sandbox/to/file? [y/n] "
       output_stream.string.must_include "Existing content:\nExisting\n---"
       output_stream.string.must_include "---\nNew content:\nNew"
     end
@@ -207,24 +207,24 @@ describe SymlinkerUI do
   describe '#file_exists' do
     it 'prints the right thing' do
       input_stream.expect :gets, "y\n"
-      ui.file_exists(File.absolute_path("sandbox/new/file"))
-      output_stream.string.must_equal "Overwrite sandbox/new/file? [y/n] "
+      ui.file_exists(File.absolute_path("sandbox/to/file"))
+      output_stream.string.must_equal "Overwrite sandbox/to/file? [y/n] "
     end
     it 'returns :overwrite when user answers with "y"' do
       input_stream.expect :gets, "y\n"
-      ui.file_exists(File.absolute_path("sandbox/new/file")).must_equal :overwrite
+      ui.file_exists(File.absolute_path("sandbox/to/file")).must_equal :overwrite
       input_stream.verify
     end
     it 'returns :dont_overwrite when user answers with "n"' do
       input_stream.expect :gets, "n\n"
-      ui.file_exists(File.absolute_path("sandbox/new/file")).must_equal :dont_overwrite
+      ui.file_exists(File.absolute_path("sandbox/to/file")).must_equal :dont_overwrite
       input_stream.verify
     end
     it 'asks again when the answer did not compute' do
       input_stream.expect :gets, "Something\n"
       input_stream.expect :gets, "y\n"
-      ui.file_exists(File.absolute_path("sandbox/new/file")).must_equal :overwrite
-      output_stream.string.must_equal "Overwrite sandbox/new/file? [y/n] Overwrite sandbox/new/file? [y/n] "
+      ui.file_exists(File.absolute_path("sandbox/to/file")).must_equal :overwrite
+      output_stream.string.must_equal "Overwrite sandbox/to/file? [y/n] Overwrite sandbox/to/file? [y/n] "
     end
   end
 end
